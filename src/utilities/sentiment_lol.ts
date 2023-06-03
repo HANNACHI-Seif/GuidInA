aa08d7afcfae9aea668683f053a6315a84a858fe
HANNACHI-Seif <shannachi2@gmail.com>
2023-06-03 13:36:32 +0100

    sentiment analyser method to execute py script :3

---
+++ src/utilities/sentiment_lol.ts
@@ -0,0 +1,23 @@
+import { exec } from 'child_process';
+
+
+let sentiment_isPositive = (text: string) => {
+    exec(`python sentiment_analysis.py "${text}"`, (error, stdout) => {
+        if (error) {
+          console.error(`Error executing the Python script: ${error}`);
+          return {
+            error
+          }
+        }
+      
+        // Parse the output from the Python script
+        const output = parseInt(stdout.trim());
+      
+        if (output == 1) return { error: null, result: true }
+        return { error: null, result: false }
+    });
+}
+
+export {
+    sentiment_isPositive
+}
